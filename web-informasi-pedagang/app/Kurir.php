88b11e5a2d31c77bb0f1a6add288d7d006e041c5
helmuth12081999 <helmuthsimon123@gmail.com>
2020-04-29 15:05:12 +0700

    Menambahkan model dan migration kurir

---
+++ web-informasi-pedagang/app/Kurir.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kurir extends Model
+{
+    protected $table = 'kurirs';
+
+    protected $primaryKey = 'id_kurir';
+
+    protected $fillable = ['jenis_kendaraan','latitude','longtitude','nomor_kendaraan','nama_kurir','alamat_kurir','nomor_telepon','nomor_ktp','foto_ktp','foto_stnk','foto_SIM','foto_kurir','jenis_kelamin','status'];
+
+}
