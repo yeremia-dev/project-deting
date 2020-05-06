05ad020e1f05cc20f25dc31cef507dd7065b59e7
JulianaTurnip03 <julianaturnip673@gmail.com>
2020-05-06 19:52:28 +0700

    halaman pengauan

--- web-informasi-pedagang/resources/views/index.blade.php
+++ web-informasi-pedagang/resources/views/index.blade.php
@@ -1,5 +1,71 @@
 @extends('layouts.master')
+
+@section('title')
+	<title>Aduan</title>
+@endsection
+
 @section('content')
-	<h1>Ini adalah halaman Index</h1>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-4">
+                        <br>
+                        <h4 class="text-center">Form Pengaduan</h4>
+                    </div>                   
+                </div>
+            </div>
+        </div>
+        <br>
+ <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="sel1">Keluhan Kepada</label>
+                                        <select class="form-control" id="Keluhan Kepada">
+                                            <option>-</option>
+                                            <option>Pedagang</option>
+                                            <option>Kurir</option>
+                                        </select>
+                                    </div>
+                                </form>
+                                 <div class="form-group">
+                                    <label for="nama_budaya">Nama Pengadu</label>
+                                    <input type="text" name="nama_budaya" class="form-control {{ $errors->has('nama_budaya') ? 'is-invalid':'' }}" id="nama_budaya" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="nama_budaya">Email Pengadu</label>
+                                    <input type="text" name="nama_budaya" class="form-control {{ $errors->has('nama_budaya') ? 'is-invalid':'' }}" id="nama_budaya" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="nama_budaya">Nomor Telepon</label>
+                                    <input type="text" name="nama_budaya" class="form-control {{ $errors->has('nama_budaya') ? 'is-invalid':'' }}" id="nama_budaya" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="nama_budaya">Lokasi</label>
+                                    <input type="text" name="nama_budaya" class="form-control {{ $errors->has('nama_budaya') ? 'is-invalid':'' }}" id="nama_budaya" required>
+                                </div>                               
+                                <div class="form-group">
+                                    <label for="foto">Foto Bukti Aduan</label>
+                                    <input type="file" name="foto" id="foto" class="form-control {{ $errors->has('foto') ? 'is-invalid':'' }}" required="">
+                                </div>
+                            </div>
+
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label for="lokasi">Isi Aduan</label>
+                                    <textarea name="lokasi" id="lokasi" cols="20" rows="20" class="form-control {{ $errors->has('lokasi') ? 'is-invalid':'' }}" required=""></textarea>
+                                </div>
+                                 <div class="align-self-end ml-auto">
+                                    <button class="btn btn-primary" style="float:right">Kirim</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            
+        </section>
+    </div>
 @endsection
 
--- web-informasi-pedagang/resources/views/layouts/nav.blade.php
+++ web-informasi-pedagang/resources/views/layouts/nav.blade.php
@@ -1,19 +1,5 @@
 <div class="header-area">
-    <div class="row align-items-center">
-                    <!-- nav and search button -->
-        <div class="col-md-6 col-sm-8 clearfix">
-            <div class="nav-btn pull-left">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <div class="search-box pull-left">
-                            <form action="#">
-                                <input type="text" name="search" placeholder="Search..." required>
-                                <i class="ti-search"></i>
-                            </form>
-                        </div>
-                    </div>
-                  
-                </div>
-            </div>
+    <div class="col-sm-6">
+        <h2 class="m-0 text-dark">Aduan</h2>
+    </div>
+</div>
