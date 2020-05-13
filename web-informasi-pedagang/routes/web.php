e08299390970216774ba97c282d6c1929aade572
Mariana Sinaga <if417028@students.del.ac.id>
2020-05-13 10:46:21 +0700

    hhhh

--- web-informasi-pedagang/routes/web.php
+++ web-informasi-pedagang/routes/web.php
@@ -9,10 +9,31 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web"  middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth','admin']], function (){
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('/role-register','Admin\DashboardController@registered');
+    Route::get('/role-edit/{id}','Admin\DashboardController@registeredit');
+    Route::put('/role-register-update/{id}','Admin\DashboardController@registerupdate');
+    Route::delete('/role-delete/{id}','Admin\DashboardController@registerdelete');
+
+    Route::get('/pengacara','Admin\PengacaraController@index');
+
+});
+
